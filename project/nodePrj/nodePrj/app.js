4605248af45815b4d9a2f639b5744799cd294413
rwk <rwkang@mail.ustc.edu.cn>
2023-05-15 22:54:51 +0800

    fix：修改moogos使用

--- project/nodePrj/nodePrj/app.js
+++ project/nodePrj/nodePrj/app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var bodyParser = require("body-parser");
+var waterFall = require("./routes/water_fall.js");
 
 //连接mongoDB
 const mongoose = require("mongoose");
@@ -17,11 +19,9 @@ db.once("open", (callback) => {
 });
 var app = express();
 
-var bodyParser = require("body-parser");
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var waterFall = require("./routes/water_fall.js");
 app.use("/water_fall", waterFall);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -34,6 +34,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
+  console.log("404");
   next(createError(404));
 });
 
--- project/nodePrj/nodePrj/routes/water_fall.js
+++ project/nodePrj/nodePrj/routes/water_fall.js
@@ -7,21 +7,33 @@ router.get(
   "/picture_list",
   function ({ query: { index = 0, count = 20 } }, res) {
     console.log("-----");
-    list.find((err, list) => {
-      if (err) {
-        console.log(err);
-      }
+    list.find().then((list) => {
+      console.log(list, "list");
       let result = list.slice(~~index, ~~index + ~~count);
       res.status(200).send(result);
     });
+    // list.find((err, list) => {
+    //   if (err) {
+    //     console.log(err);
+    //   }
+    //   let result = list.slice(~~index, ~~index + ~~count);
+    //   res.status(200).send(result);
+    // });
   }
 );
 router.post("/submitList", function (req, res) {
   const postData = req.body;
-
-  list.insertMany(postData, function (err, data) {
-    if (err) throw err;
-    res.status(200).send("成功");
-  });
+  list
+    .insertMany(postData)
+    .then(() => {
+      res.status(200).send("成功");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
+//   list.insertMany(postData, function (err, data) {
+//     if (err) throw err;
+//     res.status(200).send("成功");
+//   });
 exports = module.exports = router;
